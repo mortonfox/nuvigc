811d09d2d995510a77f79fdd2a66bd94097ba8be
Morton Fox <github@mortonfox.otherinbox.com>
2010-12-12 23:08:17 +0000

    Started the project.

---
+++ nuvigc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+nuvigc.py - This program converts a SQLite database from GSAK to a GPX file
+suitable for processing by Garmin POILoader.
+
+This is based on the original GSAK macro at
+http://geocaching.totaltechworld.com/ but rewritten in Python and updated
+to support new geocaching GPX features.
+
+Version: 0.0.1
+Author: Po Shan Cheah (morton@mortonfox.com)
+Source code: http://code.google.com/p/nuvigc/
+Created: December 12, 2010
+Last updated: December 12, 2010
+"""
+
+
+# vim:set tw=0:
